73813a2e3ea8170529d7329da1424ada4da234c5
Qwedfgr <georgiykvelidze@gmail.com>
2019-11-24 22:59:21 +0300

    добавил чтение и отправку сообщений

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-import asyncio
-import gui
-import time
-import utils
 import argparse
+import asyncio
+import logging
 import os
 import sys
-
-from aiofile import AIOFile
+import time
 from concurrent.futures import TimeoutError
-from datetime import datetime
+from contextlib import asynccontextmanager
 from socket import gaierror
+from tkinter import messagebox
+
+import aionursery
+from async_timeout import timeout
+
+import gui
+import minechat as mc
+import utils
+
+watchdog_logger = logging
+CONNECTION_TIMEOUT = 10
+PING_PONG_TIMEOUT = 15
+DELAY__PING_PONG = 15
+
+
+class InvalidToken(Exception):
+    pass
 
 
 def get_arguments_parser():
@@ -25,113 +39,113 @@ def get_arguments_parser():
 
 
 async def main():
+    queues = get_queues()
     args = utils.get_args(get_arguments_parser)
-    messages_queue = asyncio.Queue()
-    sending_queue = asyncio.Queue()
-    status_updates_queue = asyncio.Queue()
-    history_queue = asyncio.Queue()
-    await asyncio.gather(
-            restore_history(args.history, messages_queue),
-            read_msgs(args.host, args.port_reader, messages_queue, history_queue),
-            save_messages(args.history, history_queue),
-            send_msgs(args.host, args.port_writer, args.token, args.nickname, sending_queue),
-            gui.draw(messages_queue, sending_queue, status_updates_queue))
+
+    async with create_handy_nursery() as nursery:
+        nursery.start_soon(gui.draw(queues['messages_queue'], queues['sending_queue'], queues['status_updates_queue']))
+        nursery.start_soon(handle_connection(args.host, args.port_reader, args.port_writer, args.history, args.token, queues))
+        nursery.start_soon(save_messages(args.history, queues['messages_queue']))
 
 
-async def generate_msgs(queue):
+async def read_msgs(host, port, queues):
     while True:
-        queue.put_nowait(f'Ping {time.time()}')
-        await asyncio.sleep(1)
-
-
-async def read_msgs(host, port, queue, history_queue):
-    attempt = 0
-    while True:
-        try:
-            reader, writer = await asyncio.open_connection(host=host, port=port)
-            if attempt:
-                print(await write_message_to_file(args.history, 'Установлено соединение\n'))
-                attempt = 0
+        async with mc.get_connection(host, port, queues) as (reader, _):
             message = await get_message_text(reader)
-            queue.put_nowait(message)
-            history_queue.put_nowait(message)
-            #print(await write_message_to_file(args.history, message))
-
-        except (ConnectionRefusedError, ConnectionResetError, gaierror, TimeoutError):
-            attempt += 1
-            if attempt <= 3:
-                error_message = 'Нет соединения. Повторная попытка\n'
-                await write_message_to_file(args.history, error_message)
-            else:
-                error_message = 'Нет соединения. Повторная попытка через 3 сек.\n'
-                #print(await write_message_to_file(args.history, error_message))
-                await asyncio.sleep(3)
-                continue
-        finally:
-            writer.close()
+            queues['messages_queue'].put_nowait(message)
+            queues['watchdog_queue'].put_nowait('New message in chat')
+            queues['history_queue'].put_nowait(message)
 
 
 async def get_message_text(reader):
-    data = await asyncio.wait_for(reader.readline(), timeout=5)
-    message = data.decode()
-    return message
-
-
-async def write_message_to_file(file, message):
-    async with AIOFile(file, 'a') as my_file:
-        print(message)
-        await my_file.write(message)
-    return message
+    while True:
+        data = await reader.readline()
+        message = data.decode()
+        return message
 
 
 async def save_messages(filepath, queue):
     while True:
-        await write_message_to_file(filepath, await queue.get())
+        await mc.write_message_to_file(filepath, await queue.get())
 
 
-async def send_msgs(host, port, token, nickname, queue):
+async def send_msgs(reader, writer, queues):
     while True:
-        msg = await queue.get()
-        print(msg)
+        msg = await queues['sending_queue'].get()
+        queues['watchdog_queue'].put_nowait('Message sent')
+        await mc.submit_message(writer, msg)
+
+
+async def watch_for_connection(watchdog_queue):
+    while True:
         try:
-            if not nickname:
-                nickname = input('Укажите ваш ник для регистрации: ')
-            nickname = nickname.replace('\n', ' ')
-            reader, writer = await asyncio.open_connection(host=host, port=port)
-            is_authorized = False
-            if token:
-                is_authorized = await authorise(writer, reader, token, nickname)
-                if not is_authorized:
-                    print('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
-            if not is_authorized:
-                await register(writer, reader, nickname)
-            await submit_message(writer, msg)
-        finally:
-            writer.close()
+            async with timeout(CONNECTION_TIMEOUT) as cm:
+                message = await watchdog_queue.get()
+                watchdog_logger.info(message)
+        except asyncio.TimeoutError:
+            watchdog_logger.info(f"{CONNECTION_TIMEOUT}s is elapsed")
+            raise ConnectionError
 
 
-async def submit_message(writer, message):
-    message = message.replace('\n', ' ')
-    writer.write(f'{message}\n\n'.encode())
-    await writer.drain()
+async def handle_connection(host, port_reader, port_writer, history, token, queues):
+    while True:
+        try:
+            async with mc.get_connection(host, port_writer, queues) as streams:
+                async with timeout(CONNECTION_TIMEOUT):
+                    is_authorized, account_info = await mc.authorise(*streams, token)
+                    if not is_authorized:
+                        messagebox.showerror('Неизвестный токен', 'Проверьте токен или зарегистрируйте заново.')
+                        raise InvalidToken
+                    event = gui.NicknameReceived(account_info['nickname'])
+                    queues['status_updates_queue'].put_nowait(event)
+
+                async with create_handy_nursery() as nursery:
+                    nursery.start_soon(mc.restore_history(history, queues['messages_queue']))
+                    nursery.start_soon(read_msgs(host, port_reader, queues))
+                    nursery.start_soon(send_msgs(*streams, queues))
+                    nursery.start_soon(watch_for_connection(queues["watchdog_queue"]))
+                    nursery.start_soon(ping_pong(*streams, queues["watchdog_queue"]))
+
+        except (ConnectionRefusedError, ConnectionResetError, ConnectionError, asyncio.TimeoutError):
+            continue
+        else:
+            break
 
 
-async def restore_history(filename, queue):
-    async with AIOFile(filename, 'r') as afp:
-        messages = await afp.read()
-        queue.put_nowait(messages.strip())
+def get_queues():
+    list_of_queues = ['messages_queue', 'sending_queue', 'status_updates_queue', 'history_queue', 'watchdog_queue']
+    queues = {k: asyncio.Queue() for k in list_of_queues}
+    return queues
 
 
-async def authorise(writer, reader, token, nickname):
-    writer.write(f'{token}\n'.encode())
-    await reader.readline()
-    answer = (await reader.readline()).decode()
-    await reader.readline()
-    return answer != 'null\n'
+@asynccontextmanager
+async def create_handy_nursery():
+    try:
+        async with aionursery.Nursery() as nursery:
+            yield nursery
+    except aionursery.MultiError as e:
+        if len(e.exceptions) == 1:
+            raise e.exceptions[0] from None
+        raise
+
+
+async def ping_pong(reader, writer, watchdog_queue):
+    while True:
+        try:
+            async with timeout(PING_PONG_TIMEOUT):
+                writer.write("\n".encode())
+                await writer.drain()
+                await reader.readline()
+            await asyncio.sleep(DELAY_PING_PONG)
+            watchdog_queue.put_nowait("Соединение установлено. Пинг сообщение отправлено")
+
+        except gaierror:
+            watchdog_queue.put_nowait("socket.gaierror: нет интернет соединения")
+            raise ConnectionError()
 
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
-    except (KeyboardInterrupt, gui.TkAppClosed) as err:
+    except (KeyboardInterrupt, gui.TkAppClosed, InvalidToken) as err:
         sys.exit()
---
+++ minechat.py
@@ -0,0 +1,72 @@
+import asyncio
+import json
+from contextlib import asynccontextmanager
+from socket import gaierror
+
+from aiofile import AIOFile
+
+import gui
+
+
+@asynccontextmanager
+async def get_connection(host, port, queues):
+    status_updates_queue = queues['status_updates_queue']
+    reader, writer = await open_connection(host, port, queues)
+    status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.INITIATED)
+    status_updates_queue.put_nowait(gui.SendingConnectionStateChanged.INITIATED)
+    try:
+        status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.ESTABLISHED)
+        status_updates_queue.put_nowait(gui.SendingConnectionStateChanged.ESTABLISHED)
+        yield (reader, writer)
+    finally:
+        status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.CLOSED)
+        status_updates_queue.put_nowait(gui.SendingConnectionStateChanged.CLOSED)
+        writer.close()
+
+
+async def open_connection(host, port, queues):
+    attempt = 0
+    while True:
+        try:
+            reader, writer = await asyncio.open_connection(host=host, port=port)
+            queues['status_updates_queue'].put_nowait(gui.ReadConnectionStateChanged.ESTABLISHED)
+            if attempt:
+                print(await write_message_to_file(args.history, 'Установлено соединение\n'))
+                attempt = 0
+            return reader, writer
+        except (ConnectionRefusedError, ConnectionResetError, gaierror, TimeoutError):
+            attempt += 1
+            if attempt <= 3:
+                error_message = 'Нет соединения. Повторная попытка\n'
+                await write_message_to_file(args.history, error_message)
+            else:
+                error_message = 'Нет соединения. Повторная попытка через 3 сек.\n'
+                #print(await write_message_to_file(args.history, error_message))
+                await asyncio.sleep(3)
+                continue
+
+
+async def submit_message(writer, message):
+    message = message.replace('\n', ' ')
+    writer.write(f'{message}\n\n'.encode())
+    await writer.drain()
+
+
+async def restore_history(filename, queue):
+    async with AIOFile(filename, 'r') as afp:
+        messages = await afp.read()
+        queue.put_nowait(messages.strip())
+
+
+async def authorise(reader, writer, token):
+    writer.write(f'{token}\n'.encode())
+    await reader.readline()
+    answer = (await reader.readline()).decode()
+    await reader.readline()
+    return answer != 'null\n', json.loads(answer)
+
+
+async def write_message_to_file(file, message):
+    async with AIOFile(file, 'a') as my_file:
+        await my_file.write(message)
+    return message
